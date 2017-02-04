a1dbfd9bfbf22f0776955167bc2cf95153b9560a
liu <364607186@qq.com>
2017-02-04 22:26:27 +0800

    main

--- js/app/main-top.js
+++ js/app/main-top.js
@@ -1,3 +1,8 @@
-/**
- * Created by LIU on 2017/1/25.
- */
+$(function () {
+    $(".main-nb>li").on("mouseenter", function () {
+        console.log($(this).offset().left);
+        if($(this).offset().left - $(".main-nb").offset().left + 720 > 1200){
+            $(this).children("div").css("left",1200-($(this).offset().left -$(".main-nb").offset().left +720) +"px" )
+        }
+    });
+});
--- js/app/shopcar.js
+++ js/app/shopcar.js
@@ -1,12 +1,26 @@
-window.onload = function () {
-    var app = angular.module("myApp",[]);
-    app.controller("myCtrl1", function ($scope) {
-        $scope.image="../images/w1.jpg";
-        $scope.uname="Longines";
-        $scope.msg="男士军旗系列日期显示自动机械表 L47744726";
-        $scope.size="颜色 : 银色 尺码 : 36mm";
-        $scope.price="7790";
-        $scope.money="";
-        console.log("1111111");
+var app = angular.module("myApp",[]);
+app.controller("myCtrl1", function ($scope) {
+    $scope.image="../images/w1.jpg";
+    $scope.uname="Longines";
+    $scope.msg="男士军旗系列日期显示自动机械表 L47744726";
+    $scope.size="颜色 : 银色 尺码 : 36mm";
+    $scope.price="7790";
+    $scope.num=1;
+    $scope.$watch("num",function () {
+        $scope.money =  $scope.num * $scope.price
+    },true);
+    $(".s-jian").on("click", function () {
+        console.log($(".s-num").val());
+        var s = $(".s-num").val();
+        s--;
+        $(".s-num").val(s);
+        $scope.num = s;
     });
-}
+    $(".s-add").on("click", function () {
+        console.log($(".s-num").val());
+        var s = $(".s-num").val();
+        s++;
+        $(".s-num").val(s);
+        $scope.num = s;
+    });
+});
